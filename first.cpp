af29b4a5919fff054d1c119fe7df0b31166f1443
MonitKapoor <kapoor.monit@gmail.com>
2022-03-16 14:02:19 +0530

    Added class body ahead of main: class members declared

--- first.cpp
+++ first.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
-int main()
+
+class student
+{
+	private:
+		int a,b;
+Public:
+		void swap(int,int);
+};
+		int main()
 {
 	//added a comment
 	cout<<"hello world"<<endl;
